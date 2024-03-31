32bd3307f63d6219f02abce2c2608b68127c12e6
FIH-COM23a106 <user@FIH-COM23a106.fsu.edu>
2024-03-31 15:23:31 -0400

    edits bus, main gd

--- Scenes/bus.gd
+++ Scenes/bus.gd
@@ -13,9 +13,8 @@ var time = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time += delta
-	
-	if intro and :
+	time += delta 
+	if intro:
 		global_position.x += delta * speed
 		if global_position.x > intro_x:
 			intro = false
